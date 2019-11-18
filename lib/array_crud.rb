ae2b46dadb9534fff9137b102ec1187037736839
Radouane Bahi <radbahi@gmail.com>
2019-11-18 19:18:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [firstelement, secondelement, thirdelement, fourthelement]
+  ["firstelement", "secondelement", "thirdelement", "fourthelement"]
 end
 
 def add_element_to_end_of_array(array, element)
